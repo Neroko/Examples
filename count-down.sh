71f6776625df0e0f3c8142eb45dd50cfb394e4da
TJ <tj.anscombe@gmail.com>
2025-06-10 19:49:12 -0400

    Create count-down.sh

---
+++ count-down.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+clear
+
+lastday=`date -d "Aug 1" +%j`
+today=`date +%j`
+days=$(($lastday - $today))
+
+while true; do
+  clear
+  case $days in
+    0)
+      echo "Last Day!"
+      ;;
+    [0-9]*)
+      echo "$days days remaining"
+      ;;
+    -[0-9]*)
+      echo "Past Last Day!"
+      ;;
+  esac
+  echo "Press [CTRL+C] to stop.."
+  sleep 5
+done
