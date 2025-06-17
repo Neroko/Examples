31060b17342b56ef9da14b0b8c559bba5f107d5f
TJ <tj.anscombe@gmail.com>
2025-06-17 07:53:56 -0400

    Update fresh-install.sh

--- fresh-install.sh
+++ fresh-install.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+# Script made to be ran on Android in program 'UserLAnd' running a 'Debian' system.
+
 sudo apt update
 
 sudo apt upgrade
 
+# Terminal
 sudo apt install \
 	tmux \
 	htop \
 	man \
 	bash-completion
 
+# GUI Desktop
+sudo apt install \
+	lxde
